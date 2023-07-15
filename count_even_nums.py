8d0bea6bd26f4ae4faa43720aa8bc34f70f6a27a
SaiSarvagna26 <saisarvagnagarre2643@gmail.com>
2023-07-15 21:19:09 +0530

    Initial commit done

---
+++ count_even_nums.py
@@ -0,0 +1,22 @@
+def count_even_numbers(A, B):
+    result = []
+    
+    for i in B:
+        start_index, end_index = i[0], i[1]
+        count = 0
+        
+        for j in range(start_index, end_index + 1):
+            if A[j] % 2 == 0:
+                count += 1
+        
+        result.append(count)
+    
+    return result
+
+A = list(map(int,input().split()))
+B = eval(input())
+output = count_even_numbers(A, B)
+print(output) 
+
+
+
